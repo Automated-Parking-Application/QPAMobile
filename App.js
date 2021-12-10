d6351632f18ebbdcbe729d8a2a28c4846b3cbf09
Tran Hoang Giang <tranhoanggiang0708@gmail.com>
2021-12-10 16:18:34 +0700

    [GiangTH] Add README.md

---
+++ App.js
@@ -0,0 +1,50 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import 'react-native-gesture-handler';
+import AppNavContainer from './src/navigations';
+import GlobalProvider from './src/context/Provider';
+import ZocialIcon from 'react-native-vector-icons/Zocial';
+import OcticonIcon from 'react-native-vector-icons/Octicons';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicon from 'react-native-vector-icons/Ionicons';
+import FoundationIcon from 'react-native-vector-icons/Foundation';
+import EvilIcon from 'react-native-vector-icons/EvilIcons';
+import EntypoIcon from 'react-native-vector-icons/Entypo';
+import FAIcon from 'react-native-vector-icons/FontAwesome';
+import FAIcon5 from 'react-native-vector-icons/FontAwesome5';
+import SimpleLineIcon from 'react-native-vector-icons/SimpleLineIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Feather from 'react-native-vector-icons/Feather';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+
+ZocialIcon.loadFont();
+OcticonIcon.loadFont();
+MaterialIcon.loadFont();
+MaterialCommunityIcon.loadFont();
+FoundationIcon.loadFont();
+EvilIcon.loadFont();
+EntypoIcon.loadFont();
+ZocialIcon.loadFont();
+FAIcon.loadFont();
+SimpleLineIcon.loadFont();
+Feather.loadFont();
+AntDesign.loadFont();
+Fontisto.loadFont();
+Ionicon.loadFont();
+const App = () => {
+  return (
+    <GlobalProvider>
+      <AppNavContainer />
+    </GlobalProvider>
+  );
+};
+
+export default App;
